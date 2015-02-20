ef792bae0e033f7b4c892e0dbdfe65c31e33c671
Josep Boada <josep@h02e56.com>
2015-02-20 11:55:51 +0100

    handlenbars stream ok

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,31 @@
 var fs = require('fs')
 var path = require('path')
 
-var through2 = require('through2')
-
 var JSONStream = require('JSONStream')
 var handlebars = require('handlebars-stream');
 
 module.exports = function (req, res, params) {
-    
-    var template = read('index.hbs')
-	
-	layout(res)
+
+	var dataStream = readData();
+    var tmpl = readTemplate('index.hbs')
+
+    var JSONparser = JSONStream.parse('data')  
+    var ht = handlebars(tmpl)    
+ 	
+ 	layout(res)
     
     function layout (res) {
 	    res.setHeader('content-type', 'text/html');
-	    
-	    var dataStream = readData();
-	     // var parser = JSONStream.parse(['data', true])
-
-	     var tmpl = read('index.hbs')
-	     var tr = handlebars(tmpl)
-	     
-	     
-	    tr.pipe(res)
-
-	    tr.end(JSON.parse(dataStream).data)
-
-
-	    
+	    dataStream.pipe(JSONparser).pipe(ht).pipe(res)     
 	}
 
-	function read (file) {
+	function readTemplate (file) {
 	    return fs.readFileSync('./static/' + file).toString();
 	}
 
 	function readData() {
-		// return fs.createReadStream('./data/data.json')
-		return fs.readFileSync('./data/data.json')
+		return fs.createReadStream('./data/data.json',{ 
+			encoding: 'utf8'
+		})
 	}
 };
